ac795e08c2d67720ae01ff7bd2e5bc46f9cb91e5
presstee <spresti17@gmail.com>
2021-08-11 15:54:27 +0000

    While Looping

---
+++ custom.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+
+message = 'Knock, Knock... '
+answer = (input("answer: "))
+
+# if input value was equal to whos there?
+if answer == "whos there?":
+   message = "nobody"
+   
+elif answer == "whos there"
+   message = "nobody"
+   
+elif answer == "who's there?"
+   message = "nobody"
+   
+elif answer == "who's there"
+   message = "nobody"
+   
+else:
+    message = "I will come back later"
+print(message)
