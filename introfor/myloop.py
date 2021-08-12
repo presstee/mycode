5ee2a374aa931bef5b527a01429e5e25707ef7e6
presstee <spresti17@gmail.com>
2021-08-12 17:19:56 +0000

    Python and physical interfaces

---
+++ introfor/myloop.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""RZFeeser | Practice
+   Printing dictionary data stored as lists to the screen"""
+
+def main():
+    """The store I'm looking for is"""
+
+    stores = [{"name": "Nike", "Apparel": ["shoes", "shirts", "shorts"]},
+         {"name": "Walmart", "Retail": ["Literally Everything"]},
+         {"name": "Publix", "Grocery": ["Best food"]}]
+
+    print(items.get("name", "Unknown items"), end=":\n")
+                                                 
+    for item in item.get("items"):
+     print("  -", items)
+
+if __name__ == "__main__":
+    main()
---
+++ practice.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+def main():
+#create stings for function
+
+#start script
+    smelly = "the last time I was in a porta-potty,"
+    string1 = " it was very dirty and"
+    string2 = " I had to wear my shirt over my nose"
+
+    print(smelly + string1 + string2)
+
+main()#
